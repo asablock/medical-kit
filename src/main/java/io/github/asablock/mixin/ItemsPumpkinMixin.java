eb4573a8edc1f28431bc7992e5c53d96e35847ec
asablock <005cga@163.com>
2022-05-21 22:22:32 +0800

    Fix #0001

--- src/main/java/io/github/asablock/mixin/ItemsPumpkinMixin.java
+++ src/main/java/io/github/asablock/mixin/ItemsPumpkinMixin.java
@@ -4,22 +4,29 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Items.class)
-public class ItemsPumpkinMixin {
+public abstract class ItemsPumpkinMixin {
+    @Shadow
+    private static Item register(BlockItem item) {
+        throw new AssertionError();
+    }
+
     @Inject(method = "register(Lnet/minecraft/block/Block;Lnet/minecraft/item/ItemGroup;)Lnet/minecraft/item/Item;",
-            at = @At("HEAD"))
+            at = @At("HEAD"),
+            cancellable = true)
     private static void register(Block block, ItemGroup group, CallbackInfoReturnable<Item> cir) {
         if (block == Blocks.PUMPKIN) {
-            cir.setReturnValue(new BlockItem(block, new Item.Settings()
+            cir.setReturnValue(register(new BlockItem(block, new Item.Settings()
                     .group(group)
                     .food(new FoodComponent.Builder()
                             .hunger(5)
                             .saturationModifier(0.6F)
-                            .build())));
+                            .build()))));
         }
     }
 }
