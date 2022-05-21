6f6d5a6c1b7813ee7a7841bd34cd3b5f99ed4112
asablock <005cga@163.com>
2022-05-21 14:55:06 +0800

    添加成就

--- src/main/java/io/github/asablock/item/BandageItem.java
+++ src/main/java/io/github/asablock/item/BandageItem.java
@@ -1,12 +1,21 @@
 package io.github.asablock.item;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BandageItem extends Item {
     private final int level;
@@ -20,12 +29,30 @@ public class BandageItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
         if (user.getHealth() < user.getMaxHealth()) {
-            user.heal(2 * (level + 1));
-            stack.damage(1, user.getRandom(), user instanceof ServerPlayerEntity ? (ServerPlayerEntity) user : null);
+            user.heal(3.7F * (level + 1));
+            damage(stack, user);
             user.getItemCooldownManager().set(this, 200 * (level + 1));
             return TypedActionResult.success(stack);
         } else {
             return TypedActionResult.fail(stack);
         }
+    }
+
+    private void damage(ItemStack stack, PlayerEntity user) {
+        stack.damage(1, user, u -> {});
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("bandage.cooldown", 10 * (level + 1)));
+        tooltip.add(new TranslatableText("bandage.heal", 4 * (level + 1)));
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        entity.heal(3.7F * (level + 1));
+        damage(stack, user);
+        user.getItemCooldownManager().set(this, 200 * (level + 1));
+        return ActionResult.SUCCESS;
     }
 }
